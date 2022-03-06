2448daf25c917a93093b8a04a174c8318f090b38
yosou20 <yosou20@student.sdu.dk>
2022-03-06 13:22:32 +0100

    add solver automatic checkbox 3

--- src/components/GrafanaRabbitMQPage.js
+++ src/components/GrafanaRabbitMQPage.js
@@ -22,9 +22,9 @@ class GrafanaRabbitMQPage extends React.Component {
               </li>
             </ul>
             <br/>
-            <a role="button" target="_blank"
+            <a role="button" target="_blank" rel= "noopener noreferrer"
                href="https://grafana.smessie.com/d/Kn5xm-gZk/rabbitmq-overview?orgId=1&refresh=15s"
-               className="btn btn-primary" rel="noreferrer">Open RabbitMQ Grafana Dashboard</a>
+               className="btn btn-primary">Open RabbitMQ Grafana Dashboard</a>
           </div>
         </div>
       </div>
