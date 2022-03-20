0f53727a289e42e71350f695de5a1b2c928e146e
yosou20 <yosou20@student.sdu.dk>
2022-03-20 13:52:37 +0100

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //const linkElement2 = screen.findAllByText(/learn react/i);
+  expect(linkElement1).toBeInTheDocument();
+});
+
+afterEach(cleanup);
+
+test('the result should be 0', () => {
+    const { getByTestId } = render(<App />); 
+    expect(getByTestId('counter')).toHaveTextContent(0)
 });
 
