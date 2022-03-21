ab34e4d0273485e08396a3a6b1cc5825be74ed39
yosou20 <yosou20@student.sdu.dk>
2022-03-21 08:46:11 +0100

    add new dropdownBox

--- src/components/AutomaticCheck.js
+++ src/components/AutomaticCheck.js
@@ -60,6 +60,23 @@ export class AutomaticCheck extends Component {
       <div className="container">
         <h2>Solver Automatic Check </h2>
         <form onSubmit={(e) => this.save(e)}>
+
+        <div className= 'dropdown'>
+            <select className='form-select form-select-lg mb-2'
+            onChange={(e) => this.handleAutomaitcCheckBox              
+            }
+            defaultValue={this.state.value}
+            aria-label=".form-select-lg"
+            style={{ width: "40%" }}
+            
+            >
+              <option value="select-solver">Select Solver</option>
+              <option value="Solver">Solver #1</option>
+              <option value="meat">Solver #2</option>
+            </select>
+          </div>        
+
+
           <div className="form-group pb-3">
             <label for="nameInp">Name</label>
             <input
@@ -68,9 +85,10 @@ export class AutomaticCheck extends Component {
               className="form-control"
               id="nameInp"
               name="filename"
-              
+              style={{ width: "40%" }}
             />
-          </div>
+          </div>    
+
           <div className="form-group pb-2">
             <label for="contentInp">New Solver</label>
             <input
@@ -79,32 +97,33 @@ export class AutomaticCheck extends Component {
               onChange={(e) => this.parseFile(e)}
               className="form-control"
               multiple
-            />
-          </div>   
-        <h5>Solver check</h5>
-        <div className="row">
-          <div className="col-sm-4">
-            <label htmlFor="flagA">Solver automatic check</label>
-          </div>
-          <div className="col-sm-8">
-            <input
-              type="checkbox"
-              id="checkSolever"
-              onChange={(e) => this.handleAutomaitcCheckBox}
-              defaultValue={this.solverAutomaCheck}
-              name="Checksolver"
+              style={{ width: "40%" }}
             />
           </div>
-        </div>
-        <div className="btn-group mr-2" role="group">
-          <button
-            onClick={() => this.addSolver()}
-            className="btn btn-primary mt-2"
-          >
-            Add  Solver
-          </button>
-          
-        </div>
+          <h5>Solver check</h5>
+          <div className="row">
+            <div className="col-sm-4">
+              <label htmlFor="flagA">Solver automatic check</label>
+            </div>
+            <div className="col-sm-8">
+              <input
+                type="checkbox"
+                id="checkSolever"
+                onChange={(e) => this.handleAutomaitcCheckBox}
+                defaultValue={this.solverAutomaCheck}
+                name="Checksolver"
+                
+              />
+            </div>
+          </div>
+          <div className="btn-group mr-2" role="group">
+            <button
+              onClick={() => this.addSolver()}
+              className="btn btn-primary mt-2"
+            >
+              Add Solver
+            </button>
+          </div>
         </form>
       </div>
     );
